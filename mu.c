4b0b6d230ca99526e1b2a37a04b37fe4dc0cd17c
sivaprakashS <31438994+sivaprakashS@users.noreply.github.com>
2018-03-12 04:18:18 -0700

    mu.c

---
+++ mu.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int n;
+printf("Enter the number:");
+scanf("%d",&n);
+if(n%7==0)
+{
+printf("YES");
+}
+else
+{
+printf("NO");
+}
+                                                  
